ff5ab1a1fed6dcddaf3588dc93d57e3876e05874
Rail_KH <Ra1l2509@yandex.ru>
2024-12-18 21:40:16 +0500

    2

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"github.com/Rail-KH/HTTP-Calculator/internal/application"
+)
+
+func main() {
+	app := application.New()
+	app.RunServer()
+}
--- internal/application/application.go
+++ internal/application/application.go
@@ -1,1 +1,65 @@
 package application
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Rail-KH/HTTP-Calculator/pkg/calculation"
+)
+
+type Config struct {
+	Addr string
+}
+
+func ConfigFromEnv() *Config {
+	config := new(Config)
+	config.Addr = os.Getenv("PORT")
+	if config.Addr == "" {
+		config.Addr = "8080"
+	}
+	return config
+}
+
+type Application struct {
+	config *Config
+}
+
+func New() *Application {
+	return &Application{
+		config: ConfigFromEnv(),
+	}
+}
+
+type Request struct {
+	Expression string `json:"expression"`
+}
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	request := new(Request)
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := calculation.Calc(request.Expression)
+	if err != nil {
+		if errors.Is(err, calculation.ErrInvalidExpression) {
+			fmt.Fprintf(w, "err: %s", err.Error())
+		} else {
+			fmt.Fprintf(w, "unknown err")
+		}
+
+	} else {
+		fmt.Fprintf(w, "result: %f", result)
+	}
+}
+
+func (a *Application) RunServer() error {
+	http.HandleFunc("/api/v1/calculate", CalcHandler)
+	return http.ListenAndServe(":8080", nil)
+}
